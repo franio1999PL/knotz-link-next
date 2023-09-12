f3ea48551c3e8df6d89b1d2c73264366ed2cf1be
Franciszek Sikora <sikorafranek@gmail.com>
2023-09-12 02:37:36 +0200

    removed position fixed form categories slider

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -165,7 +165,7 @@ export default function Slider ({ Images }: Props) {
                       'block min-w-[195px] min-h-[197px] w-full h-full max-w-[195px] max-h-[197px] shadow-lg border-2 border-black rounded-3xl relative overflow-hidden'
                     )}
                     style={{
-                      background: `url(https://cms.bladywebdev.pl/assets/${image}.jpg) no-repeat fixed center`
+                      background: `url(https://cms.bladywebdev.pl/assets/${image}.jpg) no-repeat scroll center`
                     }}
                     // width='195'
                     // height='197'
